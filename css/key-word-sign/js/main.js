539be048f78b3c3cf135f567ac338542dbfa0188
nyaooo <nianyao817@gmail.com>
2018-01-18 16:18:16 +0800

    update

--- css/key-word-sign/js/main.js
+++ css/key-word-sign/js/main.js
@@ -1,6 +1,6 @@
 function randerKs () {
     // 获取所有调用了插件的元素
-    var _list = document.querySelectorAll('.fw-warpper');
+    var _list = document.querySelectorAll('.ks-warpper');
     console.log(_list[0].innerHTML);
 
     _list.forEach((item) => {
@@ -10,7 +10,7 @@ function randerKs () {
         var _keyWord = _str[_keyWordIndex];
 
         // 加上css
-        _str[_keyWordIndex] = "<span class='fw-key-word'>" + _keyWord + "</span><span class='fw-placeholder'></span>";
+        _str[_keyWordIndex] = "<span class='ks-key-word'>" + _keyWord + "</span><span class='ks-placeholder'></span>";
         item.innerHTML = _str.join('')
     })
 }
